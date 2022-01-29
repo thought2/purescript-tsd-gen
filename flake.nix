3d4339804729dc79d5146868324dfa5a897dd1db
Michael Bock <me@thought2.de>
2022-01-29 16:24:46 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "TypeScript Declaration File (.d.ts) generator for PureScript";
+
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            helloProject =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc8107";
+
+                shell.tools = {
+                  cabal = { };
+                  hlint = { };
+                  haskell-language-server = { };
+                };
+                shell.buildInputs = with pkgs; [
+                  nixpkgs-fmt
+                ];
+                shell.crossPlatform = p: [ p.ghcjs ];
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.helloProject.flake {
+          crossPlatforms = p: [ p.ghcjs ];
+        };
+      in
+      flake // {
+        defaultPackage = flake.packages."purescript-tsd-gen:exe:purs-tsd-gen";
+      });
+}
